0a5b92be68669ee2163e95f97cb8adb785bb37a9
Tarasov Denis <denistarasov1993@gmail.com>
2024-05-22 10:18:07 +0300

    Модуль, итерация 3, заголовок

--- store.laptop/install/components/store/laptops.list/templates/.default/lang/ru/template.php
+++ store.laptop/install/components/store/laptops.list/templates/.default/lang/ru/template.php
@@ -3,4 +3,4 @@ $MESS['LAPTOPS_LIST_MANUFACTURERS'] = 'Производители:';
 $MESS['LAPTOPS_LIST_MODELS'] = 'Модели:';
 $MESS['LAPTOPS_LIST_LAPTOPS'] = 'Ноутбуки:';
 $MESS['LAPTOPS_LIST_YEAR'] = 'год выпуска';
-$MESS['LAPTOPS_LIST_PRICE'] = 'у.е.';
+$MESS['LAPTOPS_LIST_PRICE'] = 'Цена';
